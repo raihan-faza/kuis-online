634857cb8410fa26dc7904a6ecdf5e9478f06b26
raihan-faza <raihanfazasabiqr@gmail.com>
2024-06-03 23:27:03 +0700

    adding quiz template and view

--- client/home/urls.py
+++ client/home/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-  path(''       , views.index,  name='index'),
-  path('tables/', views.tables, name='tables'),
+    path('', views.index,  name='index'),
+    path('tables/', views.tables, name='tables'),
+    path('quiz/attempt', views.attempt_quiz, name='attempt_quiz'),
+    path('submit/', views.submit_quiz, name='submit_quiz')
 ]
--- client/home/views.py
+++ client/home/views.py
@@ -8,16 +8,48 @@ from django.contrib.auth.decorators import login_required
 
 from .models import *
 
+
 def index(request):
 
-  context = {
-    'segment'  : 'index',
-    #'products' : Product.objects.all()
-  }
-  return render(request, "pages/index.html", context)
+    context = {
+        'segment': 'index',
+        # 'products' : Product.objects.all()
+    }
+    return render(request, "pages/index.html", context)
+
 
 def tables(request):
-  context = {
-    'segment': 'tables'
-  }
-  return render(request, "pages/dynamic-tables.html", context)
+    context = {
+        'segment': 'tables'
+    }
+    return render(request, "pages/dynamic-tables.html", context)
+
+
+def attempt_quiz(request):
+    context = {
+        'questions': [
+            {
+                'number': '1',
+                'question': 'test_question',
+                'choices': {
+                    'a': 'apa',
+                    'b': 'bbbb',
+                    'c': 'ccc',
+                }
+            },
+            {
+                'number': '2',
+                'question': 'test_question_2',
+                'choices': {
+                    'a': 'apa',
+                    'b': 'bbbb',
+                    'c': 'ccc',
+                }
+            },
+        ]
+    }
+    return render(request, "/home/lahh/projects/scalable/client/templates/quiz/question.html", context)
+
+
+def submit_quiz(request):
+    return
