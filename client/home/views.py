caa4533178d27de5df7b49b724be2c7fb3f85957
raihan-faza <raihanfazasabiqr@gmail.com>
2024-06-11 01:46:22 +0700

    integrating quiz creation [failed]

--- client/home/views.py
+++ client/home/views.py
@@ -20,7 +20,6 @@ from django.views.generic import CreateView
 from django.contrib.auth import logout
 
 from django.contrib.auth.decorators import login_required
-import requests
 
 from .models import *
 
@@ -97,18 +96,21 @@ def attempt_quiz(request):
     }
     return render(request, "/home/lahh/projects/scalable/client/templates/quiz/show_question.html", context)
 
+
 def auth(request):
     context = {
         'segment': 'login'
     }
     return render(request, "pages/login.html", context)
 
+
 def login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
         try:
-            response = requests.post('http://localhost:5000/auth/users/login', json={'email': email, 'password': password})
+            response = post(url='http://localhost:5000/auth/users/login',
+                            data={'email': email, 'password': password})
             if response.status_code == 200:
                 data = response.json()
                 print(data)
@@ -119,11 +121,15 @@ def login(request):
                 return redirect('auth')
         except:
             return redirect('auth')
+
+
 def create_quiz(request):
     return render(request=request, template_name="quiz/create_quiz.html")
 
+
 def leaderboard(request):
     return render(request=request, template_name="pages/Leaderboard.html")
+
 
 @require_POST
 def submit_quiz(request):
@@ -136,6 +142,12 @@ def submit_quiz(request):
         'quiz_name': quiz_name,
         'questions': []
     }
+    try:
+        response = post(url="http://localhost:8081/quiz", data={"Id": "1", "Name": f"{quiz_name}", "Description": "test desc",
+                                                                "DurationMinute": "20", "OpenTime": "2024-09-02T13:13:13.866Z", "CloseTime": "2024-10-02T13:13:13.866Z"})
+        return response.json()
+    except:
+        return JsonResponse(data={"message": "failed to create quiz"})
 
     for i, question_text in enumerate(questions):
         question_dict = {
@@ -146,8 +158,12 @@ def submit_quiz(request):
         quiz_data['questions'].append(question_dict)
 
     json_data = json.dumps(quiz_data)
-    try:
-        post(url="urlirfan", data=json_data)
+    return JsonResponse(json_data, safe=False)
+
+
+'''
+     try:
+        post(url="http://localhost:5000", data=json_data)
     except:
         return JsonResponse({"Message": "failed to create quiz"}, safe=False)
-    return JsonResponse(json_data, safe=False)
+'''
