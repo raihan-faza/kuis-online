60c629b4ace3edef8d87dd60967809ccc18690f8
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-06-10 12:50:20 +0700

    feat: Add leaderboard service to docker swarm and compose.yaml

--- leaderboard_project/leaderboard_project/settings.py
+++ leaderboard_project/leaderboard_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rs+(_vx0v1iju=xeyk9qs-oy#k51f%4t$kw5xvv(jz4bhn2+##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leaderboard', 'localhost']
 
 
 # Application definition
